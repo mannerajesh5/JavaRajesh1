79cd9f10bc7b4f4ed3166f9223ea1a7a0ce6d877
cherchita5 <mannerajesh308@gmail.com>
2023-02-03 19:25:11 +0530

    added one line

--- testJava/src/testJava/Example1.java
+++ testJava/src/testJava/Example1.java
@@ -5,6 +5,7 @@ public class Example1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Namaste Eshwara");
+		System.out.println("Namaste Narayana");
 
 	}
 
